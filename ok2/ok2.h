e3d6231451c4227e8c00ae201fd3ae10aa26f9c1
testcocoapod <nguyenphu2810.2@gmail.com>
2020-05-11 20:52:35 +0700

    ok3

---
+++ ok2/ok2.h
@@ -0,0 +1,19 @@
+//
+//  ok2.h
+//  ok2
+//
+//  Created by Phu on 5/11/20.
+//  Copyright © 2020 Phu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ok2.
+FOUNDATION_EXPORT double ok2VersionNumber;
+
+//! Project version string for ok2.
+FOUNDATION_EXPORT const unsigned char ok2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ok2/PublicHeader.h>
+
+
