388e4cb890bc473109d5f71f49fd46ebf570deaa
testcocoapod <nguyenphu2810.2@gmail.com>
2020-05-11 19:58:44 +0700

    ok

---
+++ TestCarthage/TestCarthage.h
@@ -0,0 +1,19 @@
+//
+//  TestCarthage.h
+//  TestCarthage
+//
+//  Created by Phu on 5/11/20.
+//  Copyright © 2020 Phu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestCarthage.
+FOUNDATION_EXPORT double TestCarthageVersionNumber;
+
+//! Project version string for TestCarthage.
+FOUNDATION_EXPORT const unsigned char TestCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestCarthage/PublicHeader.h>
+
+
